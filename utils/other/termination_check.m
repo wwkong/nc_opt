0c2938cb68019be4219e26ff5e46c26a6a45f1bf
William Kong <wwkong92@gmail.com>
2021-11-07 10:59:15 -0500

    Re-organization.

---
+++ utils/other/termination_check.m
@@ -0,0 +1,26 @@
+% Utility function that checks whether or not a pair (x,p) satisfies the
+% first-order stationarity condition. 
+%   - grad_constr_fn(a,b) is the gradient (or derivative) of the constraint 
+%     function at `a` evaluated with displacement b.
+%   - proj_dh(a,b) is the projection of `b` onto the subdifferential of h at `a`.
+%   - proj_NKt(a,b) is the projection of `b` onto the normal cone of of the dual 
+%     cone of K at `a`.
+function [terminate, w, q] = termination_check(x, p, oracle, constr_fn, grad_constr_fn, proj_dh, proj_NKt, norm_fn, rho, eta) 
+  
+  % Initialize.
+  o_at_x = oracle.eval(x);
+  primal_subgrad = -o_at_x.grad_f_s() - grad_constr_fn(x, p);
+  w = proj_dh(x, primal_subgrad) - primal_subgrad;
+  primal_residual = norm_fn(w);
+  dual_subgrad = constr_fn(x);
+  q = proj_NKt(p, dual_subgrad) - dual_subgrad;
+  dual_residual = norm_fn(q);
+  
+  % Check and output.
+%   disp(table(primal_residual, dual_residual));
+  if (primal_residual <= rho && dual_residual <= eta)
+    terminate = true;
+  else
+    terminate = false;
+  end
+end
