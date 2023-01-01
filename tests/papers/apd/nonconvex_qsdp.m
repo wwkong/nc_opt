bae12da434af67bd2674435d36bc1c55968ef298
Kong <wkong37@gatech.edu>
2023-01-01 11:55:54 -0500

    Better APD code

--- tests/papers/apd/nonconvex_qsdp.m
+++ tests/papers/apd/nonconvex_qsdp.m
@@ -16,12 +16,13 @@ run('../../../init.m');
 
 % Set up variables
 N = 1000;
-seed = 7;
+seed = 7777777;
 dimM = 10;
-dimN = 35;
+dimN = 20;
 density = 1.0;
-global_tol = 1e-5;
-time_limit = 2000;
+global_tol = 1e-6;
+time_limit = 2400;
+iter_limit = 1E6;
 
 % Set hyperparameters.
 base_hparam = struct();
@@ -36,9 +37,12 @@ upf_hparam.line_search_type = 'monotonic';
 apd_hparam = base_hparam;
 apd_hparam.m0 = 1;
 apd_hparam.M0 = 1;
+apd_hparam.theta = 4;
+apd_hparam.beta = 2;
+apd_hparam.alpha = 2;
 apd1_hparam = apd_hparam;
 apd1_hparam.line_search_type = 'monotonic';
-apd2_hparam = base_hparam;
+apd2_hparam = apd_hparam;
 apd2_hparam.line_search_type = 'optimistic';
 
 pgd_hparam = base_hparam;
@@ -46,20 +50,24 @@ pgd_hparam.m0 = global_tol;
 pgd_hparam.M0 = 1;
 pgd_hparam.steptype = 'adaptive';
 
-% Loop over the curvature pair (m, M).
-base = 5;
-mM_vec = [base^1, base^3;  base^1, base^4;  base^1, base^5; ...
-          base^2, base^5;  base^3, base^5;  base^4, base^5; ];
+aipp_hparam = base_hparam;
+aipp_hparam.steptype = 'aipp';
+aipp_hparam.acg_steptype = 'constant';
+aipp_hparam.sigma = 1/4;
 
-% mM_vec = [1E0, 1E5];
+% Loop over the curvature pair (m, M).
+base = 10;
+mM_vec = [base^2, base^4;  base^2, base^5;  base^2, base^6; ...
+          base^4, base^5;  base^3, base^5;  base^2, base^5; ];
 
 [nrows, ncols] = size(mM_vec);
+offset = 3;
 
-for i = 1:nrows
+for i = 1:(nrows-offset)
   % Use a problem instance generator to create the oracle and
   % hyperparameters.
-  m = mM_vec(i, 1);
-  M = mM_vec(i, 2);
+  m = mM_vec(i + offset, 1);
+  M = mM_vec(i + offset, 2);
   [oracle, hparams] =  test_fn_unconstr_02(N, M, m, seed, dimM, dimN, density);
 
   % Create the Model object and specify the solver.
@@ -69,24 +77,29 @@ for i = 1:nrows
   ncvx_qp.M = hparams.M;
   ncvx_qp.m = hparams.m;
   ncvx_qp.x0 = hparams.x0;
+  
+  % Adjustments for AIPP.
+  aipp_hparam.m = hparams.m;
+  aipp_hparam.M = hparams.M;
 
   % Set up the termination criterion.
   ncvx_qp.opt_type = 'relative';
   ncvx_qp.opt_tol = global_tol;
   ncvx_qp.time_limit = time_limit;
+  ncvx_qp.iter_limit = iter_limit;
 
   % Run a benchmark test and print the summary.
-  solver_arr = {@ECG, @UPFAG, @ADAP_FISTA, @APD};
-  hparam_arr = {pgd_hparam, upf_hparam, ncf_hparam, apd2_hparam};
-  name_arr = {'APGD', 'UPF', 'ANCF', 'APD'};
+  solver_arr = {@UPFAG, @ADAP_FISTA, @AIPP, @APD};
+  hparam_arr = {upf_hparam, ncf_hparam, aipp_hparam, apd2_hparam};
+  name_arr = {'UPF', 'ANCF', 'AIPP', 'APD'};
   
 %   solver_arr = {@UPFAG, @APD};
 %   hparam_arr = {upf_hparam, apd1_hparam};
 %   name_arr = {'UPF', 'APD1'};
 
 %   solver_arr = {@APD};
-%   hparam_arr = {apd1_hparam};
-%   name_arr = {'APD1'};
+%   hparam_arr = {apd2_hparam};
+%   name_arr = {'APD2'};
 
 %   solver_arr = {@ECG, @APD};
 %   hparam_arr = {pgd_hparam, apd1_hparam};
@@ -96,9 +109,13 @@ for i = 1:nrows
 %   hparam_arr = {ncf_hparam, apd2_hparam};
 %   name_arr = {'ANCF', 'APD2'};
   
+%   solver_arr = {@AIPP, @APD};
+%   hparam_arr = {aipp_hparam, apd2_hparam};
+%   name_arr = {'AIPP', 'APD2'};
+  
   [summary_tables, comp_models] = run_CM_benchmark(ncvx_qp, solver_arr, hparam_arr, name_arr);
   disp(summary_tables.all);
-  writetable(summary_tables.all, "adp_qsdp" + num2str(i) + ".xlsx");
+  writetable(summary_tables.all, "adp_qsdp" + num2str(i + offset) + ".xlsx");
   
   % Set up the final table.
   if (i == 1)
